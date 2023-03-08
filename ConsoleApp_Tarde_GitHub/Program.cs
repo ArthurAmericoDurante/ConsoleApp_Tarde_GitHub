bc86e58f1720953d88f3594875e9f9dc27a95ce4
Arthur <arthur.durante@fatec.sp.gov.br>
2023-03-08 16:49:50 -0300

    Criação dos métodos GET e SET (Construtor de classe) da classe Endereço.

--- ConsoleApp_Tarde_GitHub/Program.cs
+++ ConsoleApp_Tarde_GitHub/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp_Tarde_GitHub;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
 
 
 Produto p1 = new Produto(1, "Mouse xing ling", 10.90M);
@@ -11,3 +13,6 @@ p1.setValor(29.90M);
 p1.setDescricao("Mouse Razer");
 
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
+
+
+

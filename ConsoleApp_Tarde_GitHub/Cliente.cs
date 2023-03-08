70c949dc1c952d776eca07690451b3d4ce23a46f
Arthur <arthur.durante@fatec.sp.gov.br>
2023-03-08 16:16:49 -0300

    Criação do metodo GET e SET (Construtor da classe)

--- ConsoleApp_Tarde_GitHub/Cliente.cs
+++ ConsoleApp_Tarde_GitHub/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,45 @@ namespace ConsoleApp_Tarde_GitHub
     {
         private int Id;
         private string Nome;
-        private decimal Telefone;
+        private string Telefone;
+
+        public Cliente(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
     }
+
+
 }
